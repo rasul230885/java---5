4b34cd52d15f00a3e7742fec686453c2ee79f7e4
Rasul Akhmetov <rasul.akhmetov@yandex.ru>
2021-01-22 12:31:35 +0300

    great mili

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        float ticketP = 3000;
+        float bonusMile = 20;
+        float totalBonus = ticketP / bonusMile ;
+        int finalBonus = (int) totalBonus;
+        System.out.println(finalBonus);
+    }
+}
+
+
+
+
+
